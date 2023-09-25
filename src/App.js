aba06d72fa758ee56c645e6618f03676b526244b
perismuthoni <muthonimuriuki22@gmail.com>
2023-09-25 22:03:50 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes,useNavigate,Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import HelloWorld from './js/HelloWorld';
-import Somameter from './js/Somameter';
-import ViewBillPage from './js/ViewBillPage';
-import Footer from './js/ViewBillPage';
-import Header from './js/header';
-import Tariff from './js/tariff';
-import Raiseticket from "./js/ticket";
-import Login from './js/login';
-import Dashboard from './js/Dashboard';
+// import Somameter from './js/Somameter';
+// import ViewBillPage from './js/ViewBillPage';
+// import Footer from './js/ViewBillPage';
+// import Header from './js/header';
+// import Tariff from './js/tariff';
+// import Raiseticket from "./js/ticket";
+// import Login from './js/login';
+
 
 function App() {
 
-    const dummyCredentials = [
-        { username: 'LKINYUA', password: '1234' },
-        { username: 'MJOHNSON', password: '1234' },
-        { username: 'OCLARK', password: '1234' },
-        { username: 'WWANGU', password: '1234' },
-        { username: 'SOMONDI', password: '1234' },
-        { username: 'IWANJIRU', password: '1234' },
-        { username: 'DJOHN', password: '1234' }
+    // const dummyCredentials = [
+    //     { username: 'LKINYUA', password: '1234' },
+    //     { username: 'MJOHNSON', password: '1234' },
+    //     { username: 'OCLARK', password: '1234' },
+    //     { username: 'WWANGU', password: '1234' },
+    //     { username: 'SOMONDI', password: '1234' },
+    //     { username: 'IWANJIRU', password: '1234' },
+    //     { username: 'DJOHN', password: '1234' }
 
-    ];
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState('');
+    // ];
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // const [username, setUsername] = useState('');
     //const navigate = useNavigate();
 
-    const handleLogin = (enteredUsername, enteredPassword) => {
-        // Check if the entered username and password match any dummy credentials
-        const user = dummyCredentials.find(
-            (cred) => cred.username === enteredUsername && cred.password === enteredPassword
-        );
+    // const handleLogin = (enteredUsername, enteredPassword) => {
+    //     // Check if the entered username and password match any dummy credentials
+    //     const user = dummyCredentials.find(
+    //         (cred) => cred.username === enteredUsername && cred.password === enteredPassword
+    //     );
 
-        if (user) {
-            setIsLoggedIn(true);
-            setUsername(enteredUsername);
-        } else {
-            // Failed login logic (e.g., show an error message)
-            console.log('Login failed');
-        }
-    };
+    //     if (user) {
+    //         setIsLoggedIn(true);
+    //         setUsername(enteredUsername);
+    //     } else {
+    //         // Failed login logic (e.g., show an error message)
+    //         console.log('Login failed');
+    //     }
+    // };
   return (
       <Router>
         <Routes>
           <Route path="/" element={<HelloWorld/>} />
-          <Route path="/somameter" element={<Somameter/>} />
-          <Route path="/viewbill" element={<ViewBillPage/>} />
-          <Route path="/footer" element={<Footer/>} />
-          <Route path="/header" element={<Header/>} />
-          <Route path="/tariff" element={<Tariff/>} />
-          <Route path="/raiseticket" element={<Raiseticket/>} />
-          <Route
-              path="/login"
-              element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login onLogin={handleLogin} />}
+          {/*<Route path="/somameter" element={<Somameter/>} />*/}
+          {/*<Route path="/viewbill" element={<ViewBillPage/>} />*/}
+          {/*<Route path="/footer" element={<Footer/>} />*/}
+          {/*<Route path="/header" element={<Header/>} />*/}
+          {/*<Route path="/tariff" element={<Tariff/>} />*/}
+          {/*<Route path="/raiseticket" element={<Raiseticket/>} />*/}
+          {/*<Route*/}
+          {/*    path="/login"*/}
+          {/*    element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login onLogin={handleLogin} />}*/}
 
-          />
+          {/*/>*/}
 
-          <Route
-              path="/dashboard"
-              element= {isLoggedIn ? <Dashboard username={username} /> :  <Navigate to="/login" /> }
-          />
+          {/*<Route*/}
+          {/*    path="/dashboard"*/}
+          {/*    element= {isLoggedIn ? <Dashboard username={username} /> :  <Navigate to="/login" /> }*/}
+          {/*/>*/}
 
         </Routes>
       </Router>
--- src/js/HelloWorld.js
+++ src/js/HelloWorld.js
@@ -1,208 +1,119 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 import '../css/home.css';
-import companyLogo from '../images/RujwascoLogo.png'; // Replace 'company-logo.png' with the path to your image file
-import lipabill from '../images/lipabill.jpeg';
-import replacemeter from '../images/Meter-Replacement.png';
-import jisomeemeter from '../images/jisomeemeter.png';
-import reconnection from '../images/reconnection.jpeg';
-import newconnection from '../images/Get-Connected.png';
-import exhauster from '../images/exhauster.jpeg';
-import taarif from '../images/taarif.jpeg';
-import complaint from '../images/complaint.jpeg';
-import tarrif_pdf  from '../images/tariff.pdf';
-import Footer from "./footer";
-import Ticket from "./ticket";
 
-
-
-
+import mest_logo from '../images/mest_logo.png';
 
 function HelloWorld() {
+  const dummyuserdetails = [
+    {
+      username: 'PERIS',
+      fullname: 'PERIS MUTHONI MURIUKI',
+      imageurl: '../images/complaint.jpeg',
+      nationality: 'Kenyan',
+    },
+    {
+      username: 'ROY',
+      fullname: 'ROY  Junior Ricardo MOHUBU ',
+      imageurl: '../images/complaint.jpeg',
+      nationality: 'Botswana',
+    },
+  ];
 
-        const [isVisible, setIsVisible] = useState(false);
-        const [ticketIsVisible, setTicketIsVisible] = useState(false);
+  const [UserDataIsVisible, setUserDataIsVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState('');
+  const [selectedUserDetails, setSelectedUserDetails] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
-        const openPDFViewer = () => {
-            setIsVisible(true);
-        };
-        const closePDFViewer = () => {
-            setIsVisible(false);
-        };
-        const openTicketViewer = () => {
-            setTicketIsVisible(true);
-        };
-        const closeTicketViewer = () => {
-            setTicketIsVisible(false);
-        };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value.trim().toUpperCase());
+  };
 
-        return (
+  const handlesearch = () => {
+    // Check if the input value matches any username in dummyuserdetails
+    const foundUser = dummyuserdetails.find(
+      (data) => data.username === inputValue
+    );
 
+    if (foundUser) {
+      setUserDataIsVisible(true);
+      setSelectedUser(foundUser);
+    } else {
+      setUserDataIsVisible(false);
+      setSelectedUser('');
+    }
+  };
+
+
+  const handleUserClick = (user) => {
+    setUserDataIsVisible(true);
+      setSelectedUser(user);
+  };
+  
+
+  return (
+    <div>
+       <nav className="navbar">
+      <div className="nav-logo"> <img className="logo" src={mest_logo} alt="logo" /></div>
+      <ul className="nav-links">
+        <li>MEST CLASS OF 2023</li>
+      </ul>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button type="button">Search</button>
+      </div>
+    </nav>
+
+    <div className="list-container">
+        {dummyuserdetails.map((user, index) => (
+          <div key={index} onClick={() => handleUserClick(user)} className="user-list-item">
+            
+            <img src={user.imageurl} alt="Profile" />
+            <p>Name : {user.username}</p>
+            <p>Country: {user.nationality}</p>
+          </div>
+        ))}
+      </div>
+
+      
+      <div>
+       
         <div>
-            <div className="header">
-                <img src={companyLogo} alt="Company Logo" className="logo" />
-                <p className="header-text"><center> making a difference.</center>   </p>
-            </div>
-            <hr></hr>
-
-            <nav className="navbar">
-                <ul className="nav-list">
-                    <li> <a href="/">Home</a> </li>
-
-                    <li>
-                        <a href="/services">Services</a>
-                        <ul className="dropdown-menu">
-                            <li> <a href="/services"> New connection </a></li>
-                            <li> <a href="/services">Bowser Services </a></li>
-                            <li> <a href="/services">Exhauster Services </a></li>
-                            <li> <a href="/services">Meter Testing </a></li>
-                            <li> <a href="/services">Reconnection </a></li>
-                        </ul>
-                        <FontAwesomeIcon icon={faAngleDown} className="dropdown-icon" />
-                    </li>
-                    <li> <a href="/about">About us</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
-
-            <div className="container-home">
-
-
-                <div className="column customer-section">
-                    <h2><center>
-                    <strong>Self Care Portal</strong> <span className="highlight">( for account holders )</span>
-                    </center> </h2>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                            <img src={lipabill} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/login"> View/Lipa bill</a></p>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={jisomeemeter} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p>
-                                <a href="/login"> Jisomee Meter</a>
-                                <a href="/somameter"> ( How to read your meter )</a>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={reconnection} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/login">Get reconnected</a></p>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={replacemeter} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/login">  Replace Meter</a></p>
-                        </div>
-                    </div>
-
-
-
-
-                </div>
-
-                <div className="column non-customer-section">
-                    <h2> <center>
-                        <strong>To All</strong> <span className="highlight">( for both account holders & public )</span>
-                    </center>   </h2>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={newconnection} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/lipa">New Connection</a></p>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={exhauster} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/lipa"> Get Bowser/Exhauster service </a></p>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={taarif} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="#" onClick={openPDFViewer} >Know your Taarif</a></p>
-                            {isVisible && (
-                                <div className="pdf-viewer">
-                                    <button onClick={closePDFViewer}>Close</button>
-                                    <iframe src={tarrif_pdf} width="100%" height="650px" title="PDF Viewer" />
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="column-left">
-                            <div className="icon-container">
-                                <img src={complaint} alt="Company Logo" className="logo" />
-                            </div>
-                        </div>
-                        <div className="column-right">
-                            <p> <a href="/raiseticket"  > Raise complaint or Leave compliment</a></p>
-                            {ticketIsVisible && (
-                                <div className="ticket-viewer">
-                                    <button onClick={closeTicketViewer}>Exit</button>
-                                   <Ticket/>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div>
-
-                <p>other news </p>
-                <p>other news </p>
-                <p>other news </p>
-                <p>other news </p>
-                <p>other news </p>
-
-            </div>
-
-            <Footer />
+          <span>
+            <input
+              className="searchinput"
+              type="text"
+              placeholder="eg peris"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <button
+              className="searchbutton"
+              type="submit"
+              onClick={handlesearch}
+            >
+              Search
+            </button>
+          </span>
         </div>
 
-    )
+        {UserDataIsVisible && (
+          <div>
+          
+              <div className="detailed_container">
+                <button className="back_button"> Back</button>
+                <img src={selectedUser.imageurl} alt="Profile" />
+                <p> Name {selectedUser.fullname}</p>
+                <p>{selectedUser.nationality}</p>
+              </div>
+         
+
+           
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default HelloWorld;
